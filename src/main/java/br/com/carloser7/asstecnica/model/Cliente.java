43bf6c90538271c92c66b7c7ecc5ebd77af2b5c9
CarlosEReis <carlos.er7@gmail.com>
2023-07-03 23:10:30 -0300

    feat: cria model cliente

---
+++ src/main/java/br/com/carloser7/asstecnica/model/Cliente.java
@@ -0,0 +1,132 @@
+package br.com.carloser7.asstecnica.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "cliente")
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome; 
+    private String fantasia;
+    private String documento;
+
+    @Column(name = "inscricao")
+    private String inscricaoEstadual;
+
+    @Column(name = "tabela_preco")
+    private String tabelaPreco;
+    private String email;
+    private String telefone;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "tipo_pessoa")
+    private TipoCliente tipoPessoa;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getFantasia() {
+        return fantasia;
+    }
+
+    public void setFantasia(String fantasia) {
+        this.fantasia = fantasia;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public String getInscricaoEstadual() {
+        return inscricaoEstadual;
+    }
+
+    public void setInscricaoEstadual(String inscricaoEstadual) {
+        this.inscricaoEstadual = inscricaoEstadual;
+    }
+
+    public String getTabelaPreco() {
+        return tabelaPreco;
+    }
+
+    public void setTabelaPreco(String tabelaPreco) {
+        this.tabelaPreco = tabelaPreco;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public TipoCliente getTipoPessoa() {
+        return tipoPessoa;
+    }
+
+    public void setTipoPessoa(TipoCliente tipoPessoa) {
+        this.tipoPessoa = tipoPessoa;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Cliente other = (Cliente) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+}
---
+++ src/main/java/br/com/carloser7/asstecnica/model/TipoCliente.java
@@ -0,0 +1,6 @@
+package br.com.carloser7.asstecnica.model;
+
+public enum TipoCliente {
+    FISICA,
+    JURIDICA,
+}
