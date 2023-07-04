69c6ee6e6a493421ece1bd8216137a415050c197
CarlosEReis <carlos.er7@gmail.com>
2023-07-03 23:16:49 -0300

    feat: cria migration cliente

---
+++ src/main/resources/db/migration/V01__cria_tabela_insere_clientes.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS cliente (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(40) NOT NULL,
+    fantasia VARCHAR(20),
+    documento VARCHAR(14) NOT NULL,
+    inscricao VARCHAR(18),
+    tabela_preco INT,
+    email VARCHAR(40) NOT NULL,
+    telefone VARCHAR(15) NOT NULL,
+    tipo_pessoa ENUM('FISICA', 'JURIDICA') NOT NULL
+) ENGINE=InnoDB;
+
+INSERT INTO cliente (id, nome, fantasia, documento, inscricao, tabela_preco, email, telefone, tipo_pessoa) VALUES 
+(1, 'Jose Moraes Melo', NULL, '923.267.658-34', NULL, NULL, 'antonio@antonio.com.br', '(95) 67904-7031', 'FISICA'),
+(2, 'Amaral Bragança Planejamento ME', 'Amaral Planejamento', '66526565000115', '75.791.206-0', NULL, 'planejamento.amaral@geradornv.com.br', '(97) 3861-3757', 'JURIDICA')
