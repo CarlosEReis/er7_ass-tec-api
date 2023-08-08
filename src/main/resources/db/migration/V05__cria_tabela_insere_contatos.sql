4d32359cc998aa941b218e21f99b92e3381f974d
CarlosEReis <carlos.er7@gmail.com>
2023-08-08 16:34:51 -0300

    feat: cria migration de contatos

---
+++ src/main/resources/db/migration/V05__cria_tabela_insere_contatos.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS contato (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(30) NOT NULL,
+    email VARCHAR(40) NOT NULL,
+    telefone VARCHAR(15) NOT NULL,
+    departamento VARCHAR(30),
+    id_cliente INT NOT NULL,
+    FOREIGN key(id_cliente) REFERENCES cliente(id)
+) ENGINE=InnoDB;
+
+INSERT INTO contato (nome, email, telefone, departamento, id_cliente) VALUES 
+('Lucinea Rubi Bravo', 'lucinea.bravo@geradornv.com.br', '(61) 98128-2645', 'Engenharia', 1),
+('Antonieta Barboza Folly', 'antonieta.folly@geradornv.com.br', '(77) 99119-0867', 'Engenharia', 1);
