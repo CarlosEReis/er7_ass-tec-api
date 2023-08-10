504012afbdc4f18cf76a7ceab85cee8bead6dddd
CarlosEReis <carlos.er7@gmail.com>
2023-08-10 15:17:57 -0300

    feat: vincula contatoInput com chamadoInput

--- src/main/java/br/com/carloser7/asstecnica/api/model/input/ChamadoInput.java
+++ src/main/java/br/com/carloser7/asstecnica/api/model/input/ChamadoInput.java
@@ -2,10 +2,13 @@ package br.com.carloser7.asstecnica.api.model.input;
 
 import java.util.List;
 
+import jakarta.validation.constraints.NotNull;
+
 public class ChamadoInput {
 
-    private ClienteIdInput cliente;
-    private List<ItemChamadoInput> itens;
+    @NotNull private ClienteIdInput cliente;
+    @NotNull private List<ItemChamadoInput> itens;
+    @NotNull private List<ContatoInput> contatos;
 
     public ClienteIdInput getCliente() {
         return cliente;
@@ -21,6 +24,14 @@ public class ChamadoInput {
 
     public void setItens(List<ItemChamadoInput> itens) {
         this.itens = itens;
+    }
+
+    public List<ContatoInput> getContatos() {
+        return contatos;
+    }
+
+    public void setContatos(List<ContatoInput> contatos) {
+        this.contatos = contatos;
     }
     
 }
