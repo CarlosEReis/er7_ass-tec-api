a2f5c273799390be9e5cd96194ddef35dfa0ea61
CarlosEReis <carlos.er7@gmail.com>
2023-08-18 11:17:24 -0300

    chore: cria migration usuarios e permissoes

---
+++ src/main/resources/db/migration/V07__cria_tabela_insere_usuarios_permissoes.sql
@@ -0,0 +1,32 @@
+CREATE TABLE IF NOT EXISTS usuario (
+    id INT PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL,
+    email VARCHAR(30) NOT NULL,
+    senha VARCHAR(150) NOT NULL
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS permissao (
+    id INT PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL
+) Engine=InnoDB;
+
+CREATE TABLE IF NOT EXISTS usuario_permissao (
+    id_usuario INT NOT NULL,
+    id_permissao INT NOT NULL,
+    PRIMARY KEY (id_usuario, id_permissao),
+    FOREIGN KEY (id_usuario) REFERENCES usuario(id),
+    FOREIGN KEY (id_permissao) REFERENCES permissao(id)
+) ENGINE=InnoDB;
+
+INSERT INTO usuario (id, nome, email, senha) values
+(1, 'Administrador', 'admin@er7sistemas.com', '$2y$10$vqqB/1NyLFK0NTVofmfELOqlpPkx1V8Cj2pqj/lJdHMUlalpuGPOW'),
+(2, 'Usuario', 'user@er7sistemas.com', '$2y$10$60zn8XcyS89RWTs.i/FuTudXIy8qafeNhGTfbrU//.SmF4wfoFV52');
+
+INSERT INTO permissao (id, nome) values
+(1, 'ROLE_WRITE'),
+(2, 'ROLE_READ');
+
+INSERT INTO usuario_permissao (id_usuario, id_permissao) values
+(1, 1),
+(2, 2);
+
