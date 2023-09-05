2f79b941907bb37b307f318160726ecac410293e
Carlos Reis <carlos.er7@gmail.com>
2023-09-05 16:25:19 -0300

    fix: ajusta origin do cors

--- src/main/java/br/com/carloser7/asstecnica/security/CorsConfig.java
+++ src/main/java/br/com/carloser7/asstecnica/security/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://192.168.1.12:4200")
+            .allowedOrigins("http://192.168.1.11:4200")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
     
