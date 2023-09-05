f1c58feb38f473e5188a588c690c8f4b605ea953
CarlosEReis <carlos.er7@gmail.com>
2023-09-05 20:10:41 -0300

    build: move o package controller para api > controller

--- src/main/java/br/com/carloser7/asstecnica/api/controller/AutenticacaoController.java
+++ src/main/java/br/com/carloser7/asstecnica/api/controller/AutenticacaoController.java
@@ -1,4 +1,4 @@
-package br.com.carloser7.asstecnica.controller;
+package br.com.carloser7.asstecnica.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
--- src/main/java/br/com/carloser7/asstecnica/api/controller/ProdutoController.java
+++ src/main/java/br/com/carloser7/asstecnica/api/controller/ProdutoController.java
@@ -1,4 +1,4 @@
-package br.com.carloser7.asstecnica.controller;
+package br.com.carloser7.asstecnica.api.controller;
 
 import java.util.List;
 
