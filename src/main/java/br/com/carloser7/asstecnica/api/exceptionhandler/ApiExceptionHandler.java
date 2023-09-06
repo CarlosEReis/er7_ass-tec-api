46a3b05cdf823ce1d13d2dba0b1170c77c270b64
CarlosEReis <carlos.er7@gmail.com>
2023-09-06 11:32:33 -0300

    refactor: sobrescreve handleExceptionInternal

--- src/main/java/br/com/carloser7/asstecnica/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/br/com/carloser7/asstecnica/api/exceptionhandler/ApiExceptionHandler.java
@@ -10,7 +10,6 @@ import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -25,20 +24,15 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     @Autowired
     private MessageSource messageSource;
 
-
-
-    @Override
-	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
-		
-		List<ApiErro> erros = criaListaDeErros(ex.getBindingResult());
-		
-		return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
-	}
-
     @ExceptionHandler({ RecursoNaoEncontradoException.class })
     public ResponseEntity<Object> handlerClienteNaoEncontradoException(RecursoNaoEncontradoException ex, WebRequest request) {
+        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(),HttpStatus.NOT_FOUND, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatusCode statusCode, WebRequest request) {
         List<ApiErro> erros = List.of(new ApiErro(ex.getMessage(), ex.toString()));
-        return handleExceptionInternal(ex, erros, new HttpHeaders(),HttpStatus.NOT_FOUND, request);
+        return super.handleExceptionInternal(ex, erros, headers, statusCode, request);
     }
 
     private List<ApiErro> criaListaDeErros(BindingResult bindingResult) {
@@ -46,7 +40,6 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
         for (FieldError fieldError : bindingResult.getFieldErrors()) {
             var msgUsuario = this.messageSource.getMessage(fieldError, LocaleContextHolder.getLocale());
             var msgDesenvolvedor = fieldError.toString();
-
             erros.add(new ApiErro(msgUsuario, msgDesenvolvedor));
         }
         return erros;
