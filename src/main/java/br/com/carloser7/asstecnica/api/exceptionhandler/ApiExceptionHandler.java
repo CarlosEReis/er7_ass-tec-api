6eb79a55993e90d0c29d8719076bd9641acadb78
CarlosEReis <carlos.er7@gmail.com>
2023-09-08 12:24:10 -0300

    fix: trata exception handleMethodArgumentNotValid

--- src/main/java/br/com/carloser7/asstecnica/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/br/com/carloser7/asstecnica/api/exceptionhandler/ApiExceptionHandler.java
@@ -8,15 +8,12 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ExceptionHandlerExceptionResolver;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @ControllerAdvice
@@ -33,6 +30,27 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
                 ex.getMessage()
         );
         return handleExceptionInternal(ex, apiErro, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+        List<ApiErro.Field> fieldErrors = ex.getBindingResult()
+            .getFieldErrors()
+            .stream()
+            .map((e) -> {
+                String mensagem = messageSource.getMessage(e, LocaleContextHolder.getLocale());
+                return new ApiErro.Field(e.getField(), mensagem);
+            })
+            .toList();
+
+        ApiErro apiErro = new ApiErro(
+            HttpStatus.BAD_REQUEST.value(),
+            ErroType.DADOS_INVALIDOS,
+            "Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente.",
+            fieldErrors
+        );
+        return handleExceptionInternal(ex, apiErro, headers, status, request);
     }
 
     @Override
