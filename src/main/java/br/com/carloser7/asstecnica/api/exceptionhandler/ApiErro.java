29bbe35bdf6fe9412a158c83db85f11b8c7e8f99
CarlosEReis <carlos.er7@gmail.com>
2023-09-08 12:15:49 -0300

    refactor: converte class apierro para record

--- src/main/java/br/com/carloser7/asstecnica/api/exceptionhandler/ApiErro.java
+++ src/main/java/br/com/carloser7/asstecnica/api/exceptionhandler/ApiErro.java
@@ -1,51 +1,29 @@
 package br.com.carloser7.asstecnica.api.exceptionhandler;
 
-public class ApiErro {
+import com.fasterxml.jackson.annotation.JsonInclude;
 
-    private Integer status;
-    private String type;
-    private String title;
-    private String detail;
+import java.util.ArrayList;
+import java.util.List;
 
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public record ApiErro(
+        Integer status,
+        String type,
+        String title,
+        String detail,
+        List<Field> fieldErrors) {
 
-    /*@Deprecated private String msgUsuario;
-    @Deprecated private String msgDesenvolvedor;
-
-    public ApiErro (String msgUsuario, String msgDesenvolvedor){
-        this.msgUsuario = msgUsuario;
-        this.msgDesenvolvedor = msgDesenvolvedor;
-    }*/
-
-    public ApiErro(Integer status, ErroType erroType, String detail ) {
-        this.status = status;
-        this.type = erroType.getUri();
-        this.title = erroType.getTitle();
-        this.detail = detail;
+    public ApiErro(Integer status, ErroType erroType, String detail) {
+        this(status, erroType.getUri(), erroType.getTitle(), detail, new ArrayList<>());
     }
 
-    public Integer getStatus() {
-        return status;
+    public ApiErro(Integer status, ErroType erroType, String detail, List<Field> fieldErrors) {
+        this(status, erroType, detail);
+        this.fieldErrors.addAll(fieldErrors);
     }
 
-    public String getType() {
-        return type;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getDetail() {
-        return detail;
-    }
-
-    @Override
-    public String toString() {
-        return "ApiErro{" +
-                "status=" + status +
-                ", type='" + type + '\'' +
-                ", title='" + title + '\'' +
-                ", detail='" + detail + '\'' +
-                '}';
+    public record Field(String name, String message) {
     }
 }
+
+
