70bd7d550abfb8710d07e14b3685307bda2e9206
CarlosEReis <carlos.er7@gmail.com>
2023-09-12 21:42:25 -0300

    refactor: comentario da abordagem utilizada na projection

--- src/main/java/br/com/carloser7/asstecnica/domain/repository/projection/ChamadoTecnicoView.java
+++ src/main/java/br/com/carloser7/asstecnica/domain/repository/projection/ChamadoTecnicoView.java
@@ -3,16 +3,50 @@ package br.com.carloser7.asstecnica.domain.repository.projection;
 import java.time.LocalDateTime;
 
 public interface ChamadoTecnicoView {
-    
+
     Integer getId();
     LocalDateTime getDataCriacao();
     String getStatus();
-    ClienteView getCliente();
 
+    ClienteView getCliente();
     interface ClienteView {
         Integer getId();
         String getNome();
         String getDocumento();
     }
 
+    /* Esta abordagem, retorn apenas as propriedade de cliente listadas abaixo, porém monta a
+    query SQL com todos as propriedades de cliente, conforme abaixo:
+        SELECT
+            c1_0.id, c1_0.dtcriacao, c1_0.status,
+            c2_0.id,c2_0.documento,c2_0.email,c2_0.cep,c2_0.logradouro,c2_0.numero,
+            c2_0.complemento,c2_0.bairro,c2_0.cidade,c2_0.estado,c2_0.fantasia,
+            c2_0.inscricao,c2_0.nome,c2_0.tabela_preco,c2_0.telefone,c2_0.tipo_pessoa
+        FROM chamado_tec c1_0
+        LEFT JOIN cliente c2_0
+        ON c2_0.id = c1_0.id_cliente
+        LIMIT ?,?
+
+     Em uma abordagem diferente, onde os atributos estivessen no mesmo objeto(na mesma hierarquia), a query
+     teria apenas as propriedades necessárias para montar a projeção, conforme abaixo:
+
+     public interface ChamadoTecnicoView {
+        Integer getId();
+        LocalDateTime getDataCriacao();
+        String getStatus();
+
+        Integer getClienteId();
+        String getClienteNome();
+        String getClienteDocumento();
+     }
+        SELECT
+          c1_0.id,
+          c1_0.dtcriacao,
+          c1_0.status,
+          c1_0.id_cliente,
+          c2_0.nome,
+          c2_0.documento
+        FROM chamado_tec c1_0
+        LEFT JOIN cliente c2_0
+          ON c2_0.id = c1_0.id_cliente */
 }
