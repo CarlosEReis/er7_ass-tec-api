b5b5305d7604536b151e866984ae153a58ef88be
Carlos Reis <carlos.er7@gmail.com>
2023-09-19 20:14:52 +0000

    chore: ajuste na migration chamados

--- src/main/resources/db/migration/V04__cria_tabela_insere_chamado_tecnico.sql
+++ src/main/resources/db/migration/V04__cria_tabela_insere_chamado_tecnico.sql
@@ -16,14 +16,14 @@ CREATE TABLE IF NOT EXISTS ocorrencia_ct (
     FOREIGN KEY (id_chamado) REFERENCES chamado_tec(id)
 ) Engine=InnoDb;
 
-INSERT INTO chamado_tec (dtcriacao, status,id_cliente) VALUES 
-(now(), 'FILA', 1),
-(now(), 'PROCESSANDO', 2),
-(now(), 'FINALIZADO', 2);
+INSERT INTO chamado_tec (id, dtcriacao, status,id_cliente) VALUES 
+(1, '2023-08-30 11:14:29', 'FILA', 1),
+(2, '2023-08-31 10:11:29', 'PROCESSANDO', 2),
+(3, '2023-08-31 16:18:29', 'FINALIZADO', 2);
 
 INSERT INTO ocorrencia_ct (sku, status,serial, id_chamado, descricao) VALUES 
 ('ER3001','PENDENTE','35810742459021', 1,'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui sed quia consequuntur magni dolores eos qui sed quia consequunaa'),
 ('ER839800-02','AVALIANDO','44554443583669', 1,'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui sed quia consequuntur magni dolores eos qui sed quia consequunaa'),
 ('ER727','PENDENTE','58533909538718', 2,'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui sed quia consequuntur magni dolores eos qui sed quia consequunaa'),
-('ER7662','AVALIANDO','35051414240058', 2,'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui sed quia consequuntur magni dolores eos qui sed quia consequunaa');
+('ER7662','AVALIANDO','35051414240058', 3,'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui sed quia consequuntur magni dolores eos qui sed quia consequunaa');
 
