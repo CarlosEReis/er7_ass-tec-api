148fb1b73b9b7da9d477dab6df712da23909bfc5
CarlosEReis <carlos.er7@gmail.com>
2023-10-20 16:13:26 -0300

    feat: cria os listeners para chamado processando e concluido

--- src/main/java/br/com/carloser7/asstecnica/domain/listener/ChamadoCriadoListener.java
+++ src/main/java/br/com/carloser7/asstecnica/domain/listener/ChamadoCriadoListener.java
@@ -1,6 +1,8 @@
 package br.com.carloser7.asstecnica.domain.listener;
 
+import br.com.carloser7.asstecnica.domain.event.ChamadoConcluidoEvent;
 import br.com.carloser7.asstecnica.domain.event.ChamadoCriadoEvent;
+import br.com.carloser7.asstecnica.domain.event.ChamadoProcessandoEvent;
 import br.com.carloser7.asstecnica.domain.model.Contato;
 import br.com.carloser7.asstecnica.domain.service.CadastroChamadoTecnicoService;
 import br.com.carloser7.asstecnica.infra.EmailService;
@@ -9,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class ChamadoCriadoListener  {
@@ -25,21 +29,26 @@ public class ChamadoCriadoListener  {
 
         var id = event.getChamado().getId();
         var cliente = event.getChamado().getCliente().getNome();
-        var contatos = event.getChamado()
-                            .getContatos()
-                                .stream()
-                                .map(Contato::getEmail)
-                                .toList();
+        var contatos = getContatosEmail(event.getChamado().getContatos());
         var mensagem = """
                 Olá,<br>
                 <br>
-                Chamado de id %s foi criado para o cliente <strong> %s</strong>. <br>
+                Chamado de id %s para o cliente <strong> %s</strong>, foi criado com sucesso. <br>
+  
                 <br>
                 O mesmo se encontra na <strong>fila de espera</strong>, e logo será avaliado por um de nossos analistas técnicos.
+                <br>
+                <br>
+                <i>Atenciosamente,</i>
+                <br>
+                <span>Equipe</span>
+                <br>
+                <strong>ER7 SISTEMAS</strong>
+                
                 """
                 .formatted(id, cliente);
 
-        var assunto = "ER7 Sistemas: Chamado %s aberto".formatted(id);
+        var assunto = "ER7 Sistemas \uD83D\uDFE0: Chamado %s ABERTO".formatted(id);
         var fichaTecnica = chamadoTecnicoService.relatorioFichaChamadoTecnico(event.getChamado());
 
         this.emailService.enviarEmailComAnexo(
@@ -50,4 +59,67 @@ public class ChamadoCriadoListener  {
                 fichaTecnica );
     }
 
+
+    @Async
+    @EventListener
+    public void aoIniciarAvaliacaoChamado(ChamadoProcessandoEvent event) {
+
+        var id = event.getChamado().getId();
+        var contatos = getContatosEmail(event.getChamado().getContatos());
+        var assunto = "ER7 Sistemas \uD83D\uDFE1: Chamado %s PROCESSANDO".formatted(id);
+        var mensagem = """
+                Olá,<br>
+                <br>
+                Estamos iniciando a avalização do chamado téncico (%s), e assim que finalizado, você será notificado,
+                e receberá o laudo técnico.<br>
+                
+                <br>
+                <br>
+                <i>Atenciosamente,</i>
+                <br>
+                <span>Equipe</span>
+                <br>
+                <strong>ER7 SISTEMAS</strong>
+                """
+                .formatted(id);
+
+        this.emailService.enviarEmail("carlos.er7@hotmail.com", contatos, assunto, mensagem);
+    }
+
+    @Async
+    @EventListener
+    public void aoConcluirAvaliacaoChamado(ChamadoConcluidoEvent event) throws JRException {
+
+        var id = event.getChamado().getId();
+        var cliente = event.getChamado().getCliente().getNome();
+        var contatos = getContatosEmail(event.getChamado().getContatos());
+        var assunto = "ER7 Sistemas \uD83D\uDFE2: Chamado %s AVALIACAO CONCLUÍDA ".formatted(id);
+        var mensagem = """
+                Olá,<br>
+                <br>
+                Concluímos a avaliação do chamado de id %s para o cliente <strong> %s</strong>. <br>
+                <br>
+                Abaixo está o anexo com o laudo técnico. Caso tenha alguma dúvida referente á posição técnica, não deixe
+                 de entrar em contato atraveś dos nossos canais de atendimento.
+                <br>
+                <br>
+                <i>Atenciosamente,</i>
+                <br>
+                <span>Equipe</span>
+                <br>
+                <strong>ER7 SISTEMAS</strong>
+                """
+                .formatted(id, cliente);
+
+        var fichaTecnica = chamadoTecnicoService.relatorioFichaChamadoTecnico(event.getChamado());
+
+        this.emailService.enviarEmailComAnexo("carlos.er7@hotmail.com", contatos, assunto, mensagem, fichaTecnica);
+    }
+
+    private static List<String> getContatosEmail(List<Contato> contatos) {
+        return contatos
+                .stream()
+                .map(Contato::getEmail)
+                .toList();
+    }
 }
