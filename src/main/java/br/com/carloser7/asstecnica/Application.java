3da53235b939455cfcc0fb09509d0d8616b446ca
CarlosEReis <carlos.er7@gmail.com>
2023-11-28 14:30:09 -0300

    feat: ativa seguranca nos metodos de controller

--- src/main/java/br/com/carloser7/asstecnica/Application.java
+++ src/main/java/br/com/carloser7/asstecnica/Application.java
@@ -3,8 +3,10 @@ package br.com.carloser7.asstecnica;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @EnableAsync
+@EnableMethodSecurity
 @SpringBootApplication
 public class Application {
 
