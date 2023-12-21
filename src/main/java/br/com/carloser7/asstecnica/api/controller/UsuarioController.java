a728be78d52e35f0a4da70f6ac85f43293779c12
CarlosEReis <carlos.er7@gmail.com>
2023-12-21 19:11:13 -0300

    feat: add busca por id e atualizacao de usuario

--- src/main/java/br/com/carloser7/asstecnica/api/controller/UsuarioController.java
+++ src/main/java/br/com/carloser7/asstecnica/api/controller/UsuarioController.java
@@ -5,12 +5,15 @@ import br.com.carloser7.asstecnica.domain.model.Permissao;
 import br.com.carloser7.asstecnica.domain.model.Usuario;
 import br.com.carloser7.asstecnica.domain.repository.PermissaoRepository;
 import br.com.carloser7.asstecnica.domain.repository.UsuarioRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
@@ -34,10 +37,31 @@ public class UsuarioController {
         return toUsuarioDTO(this.usuarioRepository.save(usuario));
     }
 
+    @PutMapping("/{usuarioId}")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public ResponseEntity<UsuarioDTO> adicionar(@PathVariable Integer usuarioId, @RequestBody UsuarioDTO usuarioDTO) {
+
+        // TODO FIX: Tratar para que, caso exista apenas um usuário ADMIN, o mesmo não seja alterado sua permissao
+
+        Optional<Usuario> usuarioDB = this.usuarioRepository.findById(usuarioId);
+        if (usuarioDB.isEmpty()) return ResponseEntity.notFound().build();
+        BeanUtils.copyProperties(usuarioDTO, usuarioDB.get(), "id","senha");
+        var usuarioAtualizado = this.usuarioRepository.save(usuarioDB.get());
+        return ResponseEntity.ok(toUsuarioDTO(usuarioAtualizado));
+    }
+
     @GetMapping
     @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public List<UsuarioDTO> listar() {
         return this.toCollectionUsuarioDTO(this.usuarioRepository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public ResponseEntity<UsuarioDTO> bucar(@PathVariable Integer id) {
+        var usuario = this.usuarioRepository.findById(id);
+        if (usuario.isEmpty()) return ResponseEntity.notFound().build();
+        return ResponseEntity.ok(this.toUsuarioDTO(usuario.get()));
     }
 
     @GetMapping("/permissoes")
