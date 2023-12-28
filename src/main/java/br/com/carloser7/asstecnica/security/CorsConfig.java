78f0015b514094dde8c60faf8f500e695c54fe23
CarlosEReis <carlos.er7@gmail.com>
2023-12-27 22:48:21 -0300

    build: injeta o cors na classe de config

--- src/main/java/br/com/carloser7/asstecnica/security/CorsConfig.java
+++ src/main/java/br/com/carloser7/asstecnica/security/CorsConfig.java
@@ -1,16 +1,20 @@
 package br.com.carloser7.asstecnica.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-    
+
+    @Value("${api.security.cors.origin}")
+    private String origins;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://192.168.1.8:4200")
+            .allowedOrigins(this.origins)
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
     
