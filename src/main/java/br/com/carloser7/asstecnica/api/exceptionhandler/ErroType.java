bcbe09923477042bd7fc6f88d81d18da69bf7b7b
CarlosEReis <carlos.er7@gmail.com>
2024-01-23 12:06:50 -0300

    refactor: update ErrorType

--- src/main/java/br/com/carloser7/asstecnica/api/exceptionhandler/ErroType.java
+++ src/main/java/br/com/carloser7/asstecnica/api/exceptionhandler/ErroType.java
@@ -2,6 +2,7 @@ package br.com.carloser7.asstecnica.api.exceptionhandler;
 
 public enum ErroType {
 
+    ENTIDADE_EM_USO("/entidade-em-uso", "Entidade em uso."),
     RECURSO_NAO_ENCONTRADO("/recurso-nao-encontrado", "Recurso não encontrado."),
     DADOS_INVALIDOS("/dados-invalidos", "Dados inválidos."),
     OPERACAO_NAO_PERMITIDA("/operacao-nao-permitida", "Não é possível executar esta operação.");
