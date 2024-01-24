0982a6276c8f902441d333e5f3489e351cf225c3
CarlosEReis <carlos.er7@gmail.com>
2024-01-24 14:06:33 -0300

    refactor: produto delega metodos do controller para service

--- src/main/java/br/com/carloser7/asstecnica/api/controller/ProdutoController.java
+++ src/main/java/br/com/carloser7/asstecnica/api/controller/ProdutoController.java
@@ -1,63 +1,48 @@
 package br.com.carloser7.asstecnica.api.controller;
 
-import java.util.List;
-import java.util.Optional;
-
-import jakarta.websocket.server.PathParam;
-import org.springframework.beans.BeanUtils;
+import br.com.carloser7.asstecnica.domain.model.Produto;
+import br.com.carloser7.asstecnica.domain.service.CadastroProdutoService;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
-
-import br.com.carloser7.asstecnica.domain.model.Produto;
-import br.com.carloser7.asstecnica.domain.repository.ProdutoRepository;
 
 @RestController
 @RequestMapping("/produtos")
 public class ProdutoController {
-    
-    @Autowired
-    private ProdutoRepository produtoRepository;
+
+    @Autowired private CadastroProdutoService produtoService;
 
     @GetMapping
     @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_GESTOR', 'ROLE_TECNICO')")
     public ResponseEntity<Page<Produto>> listar(@RequestParam(required = false) String search, Pageable pageable) {
-       // if (StringUtils.hasText(sku)) {
-            var result = this.produtoRepository.findBySkuContainingOrNomeContaining(search, search, pageable);
-        //}
+        var result = this.produtoService.pesquisa(search, search, pageable);
         return ResponseEntity.ok(result);
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/{produtoID}")
     @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_GESTOR', 'ROLE_TECNICO')")
-    public ResponseEntity<Produto> buscarPorId(@PathVariable Integer id) {
-        Optional<Produto> produto = this.produtoRepository.findById(id);
-        return produto.isPresent() ? ResponseEntity.ok(produto.get()) : ResponseEntity.notFound().build();
+    public Produto buscarPorId(@PathVariable Integer produtoID) {
+        return this.produtoService.buscaPorIDOuFalha(produtoID);
     }
 
     @PostMapping
     @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_GESTOR')")
-    public ResponseEntity<Produto> criar(@RequestBody Produto produto) {
-        Produto produtoSalvo = this.produtoRepository.save(produto);
-        return ResponseEntity.status(HttpStatus.CREATED).body(produtoSalvo);
+    public ResponseEntity<Produto> criar(@RequestBody Produto produto, HttpServletResponse response) {
+        produto = this.produtoService.adiciona(produto, response);
+        return ResponseEntity.status(HttpStatus.CREATED).body(produto);
     }
 
-    @PutMapping("/{id}")
+    @PutMapping("/{produtoID}")
     @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_GESTOR')")
-    public Produto atualizar(@PathVariable Integer id, @RequestBody Produto produto) {
-        Produto produdoBanco = this.produtoRepository
-            .findById(id)
-            .orElseThrow(() -> 
-                new EmptyResultDataAccessException("Produto com o id " + id + " não foi encontrado", 1));
-
-        BeanUtils.copyProperties(produto, produdoBanco, "id");
-        return this.produtoRepository.save(produdoBanco);
+    public Produto atualizar(@PathVariable Integer produtoID, @RequestBody @Valid Produto produto) {
+        produto = this.produtoService.atualizar(produtoID, produto);
+        return produto;
     }
 
 }
