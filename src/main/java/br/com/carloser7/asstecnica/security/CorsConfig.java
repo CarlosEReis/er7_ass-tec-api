028a6085928c913e0c0aa809143736c59b6c1c1f
CarlosEReis <carlos.er7@gmail.com>
2025-07-20 19:41:24 -0300

    refactor: add cors logging

--- src/main/java/br/com/carloser7/asstecnica/security/CorsConfig.java
+++ src/main/java/br/com/carloser7/asstecnica/security/CorsConfig.java
@@ -1,5 +1,7 @@
 package br.com.carloser7.asstecnica.security;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,11 +10,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(CorsConfig.class);
+
     @Value("${api.security.cors.origin}")
     private String origins;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        LOGGER.info("Configuring CORS with allowed origins: {}", this.origins);
         registry.addMapping("/**")
             .allowedOrigins(this.origins)
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
