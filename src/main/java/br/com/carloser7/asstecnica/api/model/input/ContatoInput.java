c729e1cf231a7df4ee129e66efbf2e95062097d7
CarlosEReis <carlos.er7@gmail.com>
2025-07-23 11:43:13 -0300

    refactor: converte ContatoInput de record para classe

--- src/main/java/br/com/carloser7/asstecnica/api/model/input/ContatoInput.java
+++ src/main/java/br/com/carloser7/asstecnica/api/model/input/ContatoInput.java
@@ -1,10 +1,41 @@
 package br.com.carloser7.asstecnica.api.model.input;
 
-public record ContatoInput (
-    
-    Integer id,
-    String nome,
-    String email,
-    String telefone
-    
-) {}
+public class ContatoInput {
+
+    private String nome;
+    private String email;
+    private String telefone;
+    private String departamento;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+}
