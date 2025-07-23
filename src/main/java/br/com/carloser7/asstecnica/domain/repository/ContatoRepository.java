b78e86583d47ba62edf91698f795fe350b1196a0
CarlosEReis <carlos.er7@gmail.com>
2025-07-23 11:45:12 -0300

    refactor: add metodo findByClienteId no ContatoRepository

--- src/main/java/br/com/carloser7/asstecnica/domain/repository/ContatoRepository.java
+++ src/main/java/br/com/carloser7/asstecnica/domain/repository/ContatoRepository.java
@@ -5,6 +5,10 @@ import br.com.carloser7.asstecnica.domain.model.Contato;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ContatoRepository extends JpaRepository<Contato, Integer> {
+
+    List<Contato> findByClienteId(Integer clienteId);
 }
