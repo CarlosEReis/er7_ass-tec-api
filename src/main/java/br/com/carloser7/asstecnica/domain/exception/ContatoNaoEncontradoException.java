2c9c81ebc137ea5a5142bdd2b7a3906b214680db
CarlosEReis <carlos.er7@gmail.com>
2025-07-23 11:43:52 -0300

    refactor: add novo construtor na exception de contato

--- src/main/java/br/com/carloser7/asstecnica/domain/exception/ContatoNaoEncontradoException.java
+++ src/main/java/br/com/carloser7/asstecnica/domain/exception/ContatoNaoEncontradoException.java
@@ -3,9 +3,14 @@ package br.com.carloser7.asstecnica.domain.exception;
 public class ContatoNaoEncontradoException extends RecursoNaoEncontradoException {
 
     public static final String CONTATO_INEXISTENTE = "Contato com o id %s não existe";
+    public static final String CONTATO_INEXISTENTE_CLIENTE = "Contato de id %s não existe no cliente de id %s";
 
     public ContatoNaoEncontradoException(Integer contatoId) {
         super(String.format(CONTATO_INEXISTENTE, contatoId));
+    }
+
+    public ContatoNaoEncontradoException(Integer clienteId, Integer contatoId) {
+        super(String.format(CONTATO_INEXISTENTE_CLIENTE, contatoId,clienteId));
     }
 
 }
